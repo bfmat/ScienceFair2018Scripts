ef7477e8c47af0f175980ee77c24101a99c7f598
brendon-ai <brendon-m@outlook.com>
2017-11-11 11:06:58 -0500

    Display legend, refactored parabola drawing code to better deal with small scales

--- test_data_graph.py
+++ test_data_graph.py
@@ -17,8 +17,8 @@ def int_or_float(string):
 # Descriptions for the general concepts of the two attributes of the graph
 ATTRIBUTE_NAMES = ['parameter', 'performance metric']
 
-# The X position step size used when calculating points on the polynomials for graphing
-POLYNOMIAL_STEP_SIZE = 0.05
+# The number of X position steps used when calculating points on the polynomials for graphing
+POLYNOMIAL_STEP_COUNT = 50
 
 # Lists to hold the names and lists of elements of the parameter and performance metric
 names = []
@@ -43,27 +43,50 @@ for attribute_name in ATTRIBUTE_NAMES:
 names = [name.strip() for name in names]
 all_elements = [[element.strip() for element in elements] for elements in all_elements]
 
-# Convert all of the elements to numbers, skipping empty lines with zero length
-all_elements_numeric = [[int_or_float(element) for element in elements if len(element)] for elements in all_elements]
+# Try to convert all of the elements to numbers, skipping empty lines with zero length
+try:
+    all_elements_numeric = [[int_or_float(element) for element in elements if len(element)]
+                            for elements in all_elements]
+
+# If it fails, print an error message and exit
+except ValueError:
+    print('Non-numeric or incorrectly formatted input.')
+    sys.exit()
 
 # If the lengths of the two lists are different, exit with an error message
 if len(all_elements_numeric[0]) != len(all_elements_numeric[1]):
     print('Unequal number of', ATTRIBUTE_NAMES[0], 'points and', ATTRIBUTE_NAMES[1], 'points.')
     sys.exit()
 
+# Prompt the user to enter the title of the graph
+print('Enter the title of this graph.')
+title = input()
+
+# Set the title of the window
+plt.figure('Test Data Graphing Tool')
+
+# Set the title of the graph inside the window based on the user's input
+plt.title(title)
+
 # Plot a graph displaying the parameter and performance metric points, using square points
-plt.plot(*all_elements_numeric, 's')
+plt.plot(*all_elements_numeric, 's', label='Data points')
 
 # Fit a line (degree 1) and a parabolic curve (degree 2) to the points
 polynomials = [np.polyfit(*all_elements_numeric, deg=degree) for degree in [1, 2]]
 
 # Get the first and last X positions of the data to use as outer bounds for the points on the polynomials
 data_x_positions = all_elements_numeric[0]
-first_x_position = data_x_positions[0]
-last_x_position = data_x_positions[-1]
+min_x_position = min(data_x_positions)
+max_x_position = max(data_x_positions)
 
-# Iterate over the polynomials
-for polynomial in polynomials:
+# Calculate the step size to use based on the count and outer bounds
+polynomial_step_size = (max_x_position - min_x_position) / POLYNOMIAL_STEP_COUNT
+
+# Generate descriptions for the legend corresponding to the polynomials
+descriptions = [name + ' of best fit' for name in ['Line', 'Parabola']]
+
+# Iterate over the polynomials and corresponding descriptions
+for polynomial, description in zip(polynomials, descriptions):
 
     # Reverse the array of polynomial parameters so that the lowest order comes first
     polynomial = np.flip(polynomial, 0)
@@ -73,7 +96,7 @@ for polynomial in polynomials:
     y_positions = []
 
     # Iterate over the X positions within the same range as the data, using the predefined step size
-    for x_position in np.arange(first_x_position, last_x_position, POLYNOMIAL_STEP_SIZE):
+    for x_position in np.arange(min_x_position, max_x_position, polynomial_step_size):
         # For a polynomial of any order, calculate the Y position given the X position and polynomial parameters
         y_position = sum([(x_position ** order) * polynomial[order] for order in range(len(polynomial))])
 
@@ -81,8 +104,11 @@ for polynomial in polynomials:
         x_positions.append(x_position)
         y_positions.append(y_position)
 
-    # Plot the points in this polynomial
-    plt.plot(x_positions, y_positions)
+    # Plot the points in this polynomial, and label it with the description
+    plt.plot(x_positions, y_positions, label=description)
+
+# Display a legend on the plot
+plt.legend()
 
 # Set the X and Y labels to the provided parameter and performance metric names
 parameter_name, performance_metric_name = names
