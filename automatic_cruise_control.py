148193b8b3ba302b1afbb010dd0a24686e8ada23
brendon-ai <brendon-m@outlook.com>
2017-12-06 13:52:07 -0500

    Search for the closest object within a given angle of the center rather than only straight ahead

--- automatic_cruise_control.py
+++ automatic_cruise_control.py
@@ -8,6 +8,12 @@ from sweeppy import Sweep
 # The file path to the LIDAR device
 LIDAR_DEVICE_PATH = '/dev/cu.usbserial-DM00KERB'
 
+# The number of millidegrees in a full rotation
+FULL_ROTATION_ANGLE = 360 * 1000
+
+# The angle in millidegrees away from the center within which a vehicle ahead is searched for
+SEARCH_ANGLE = 5000
+
 # The speed to travel at if there is no vehicle ahead within a certain range
 DEFAULT_SPEED = 0.5
 
@@ -27,18 +33,32 @@ with Sweep(LIDAR_DEVICE_PATH) as sweep:
 
     # Iterate over the data stream provided by the sensor
     for scan in sweep.get_scans():
-        # Get the sample distance closest to the beginning of the rotation
-        # The rotation starts and ends at the front of the sensor, opposite the cable
-        initial_distance = scan.samples[0].distance
+        # Get the lowest distance to the car ahead within the predefined search angle of the center
+        # Create an empty variable to store the lowest distance, starting at the maximum integer value
+        closest_distance_within_search_angle = None
+
+        # Iterate over the samples in the current scan
+        for sample in scan.samples:
+
+            # Get the distance and angle from the sample
+            distance = sample.distance
+            angle = sample.angle
+
+            # If the sample's angle is within the permissible range in either direction
+            if angle <= SEARCH_ANGLE or angle >= FULL_ROTATION_ANGLE - SEARCH_ANGLE:
+                # If either the distance has not been set or the current distance is less than the closest one so far
+                if closest_distance_within_search_angle is None or distance < closest_distance_within_search_angle:
+                    # Set the closest distance to the current one
+                    closest_distance_within_range = distance
 
         # If the distance to the car ahead is greater than the distance at which we need to go slowly
         # or it is 1 centimeter, which represents a distance larger than that which can be measured
-        if initial_distance > 500 or initial_distance == 1:
+        if closest_distance_within_search_angle > 500 or closest_distance_within_search_angle == 1:
             # Set the speed to the default cruising speed
             speed = DEFAULT_SPEED
 
         # If it is within the range at which is should stop
-        elif initial_distance <= STOP_RANGE:
+        elif closest_distance_within_search_angle <= STOP_RANGE:
             # Set the speed to zero
             speed = 0
 
@@ -47,7 +67,7 @@ with Sweep(LIDAR_DEVICE_PATH) as sweep:
         else:
             # We need to linearly interpolate the speed between the cruising speed and zero
             # Calculate how far we are away from having to stop
-            distance_past_stop_range = initial_distance - STOP_RANGE
+            distance_past_stop_range = closest_distance_within_search_angle - STOP_RANGE
 
             # Calculate the distance between the upper bound of the slow range and the upper bound of the stop range
             # This is the range within which we will interpolate the speed
