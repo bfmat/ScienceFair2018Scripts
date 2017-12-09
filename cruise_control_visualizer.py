cd0dce6fb99dc7a685a31adcad4ff5d5eede190c
brendon-ai <brendon-m@outlook.com>
2017-12-09 11:04:02 -0500

    Properly implemented the background data thread

--- cruise_control_visualizer.py
+++ cruise_control_visualizer.py
@@ -2,38 +2,67 @@
 
 import sys
 
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QWidget, QApplication
 
 from automatic_cruise_control import automatic_cruise_control
-
 
 # A visualization tool for the cruise control system that displays the environmental point map on screen and the
 # calculated speed of the vehicle
 # Created by brendon-ai, December 2017
 
+# Height and width of the visualizer window
+WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 480
+
+
 # Main PyQt5 QWidget class
 class CruiseControlVisualizer(QWidget):
-
-    # Prepare the UI and run the main loop
+    # Prepare the UI and data thread and run the main loop
     def __init__(self):
         # Call the QWidget initializer
         super(CruiseControlVisualizer, self).__init__()
 
+        # Create an instance of the data thread that takes information from the LIDAR in the background
+        data_thread = CruiseControlVisualizerDataThread()
+        # Connect the data signal to the UI update function in the main thread
+        data_thread.data_signal.connect(self.update_ui)
+        # Start the data thread
+        data_thread.start()
+
         # Initialize the user interface
         self.init_ui()
-        # Start the main loop
-        self.main_loop()
 
     # Create the window and all elements within it
     def init_ui(self):
+        # Set the window's size and title
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.setWindowTitle('Cruise Control Visualizer')
         # Display the window on screen
         self.show()
 
-    # Run forever, accepting values from the LIDAR and displaying them on screen
-    def main_loop(self):
+    # Update the user interface using data retrieved from the data thread
+    def update_ui(self, data):
+        # Unwrap the data tuple
+        speed, samples = data
+        print(speed)
+
+
+# The data thread that runs forever, accepting values from the LIDAR and displaying them on screen
+class CruiseControlVisualizerDataThread(QThread):
+    # The signal that is called when it is time to transfer data to the main thread
+    data_signal = pyqtSignal(tuple)
+
+    # Initializer containing nothing but a call to the QThread initializer
+    def __init__(self):
+        QThread.__init__(self)
+
+    # The function containing the logic of the main loop
+    def run(self):
         # Iterate over the automatic cruise control generator
-        for speed, samples in automatic_cruise_control():
-            print(speed)
+        for data in automatic_cruise_control():
+            # Send the speed and samples through the signal to the UI thread
+            self.data_signal.emit(data)
 
 
 # If this file is being run directly, instantiate the ManualSelection class
