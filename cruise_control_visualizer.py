427dd53f3e3c8b3a4ae1b8a1a6e9afa4af8c4128
brendon-ai <brendon-m@outlook.com>
2017-12-09 19:09:49 -0500

    Display data in the window, using fake data

--- cruise_control_visualizer.py
+++ cruise_control_visualizer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import math
+import random
 import sys
 
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QPoint
+from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QWidget, QApplication
+from sweeppy import Sample
 
 from automatic_cruise_control import automatic_cruise_control
 
@@ -11,13 +15,21 @@ from automatic_cruise_control import automatic_cruise_control
 # calculated speed of the vehicle
 # Created by brendon-ai, December 2017
 
-# Height and width of the visualizer window
-WINDOW_WIDTH = 720
-WINDOW_HEIGHT = 480
+# Side length of the square visualizer window
+WINDOW_SIDE_LENGTH = 600
+
+# Scaling factor for mapping distances from the LIDAR sensor onto the screen, in pixels per centimeter
+DISTANCE_SCALING_FACTOR = 0.05
+
+# Diameter of the sample points drawn in the window
+SAMPLE_POINT_DIAMETER = 3
 
 
 # Main PyQt5 QWidget class
 class CruiseControlVisualizer(QWidget):
+    # List of points in the window corresponding to the samples collected by the LIDAR
+    sample_points = []
+
     # Prepare the UI and data thread and run the main loop
     def __init__(self):
         # Call the QWidget initializer
@@ -28,7 +40,7 @@ class CruiseControlVisualizer(QWidget):
         # Connect the data signal to the UI update function in the main thread
         data_thread.data_signal.connect(self.update_ui)
         # Start the data thread
-        data_thread.start()
+        # data_thread.start()
 
         # Initialize the user interface
         self.init_ui()
@@ -36,16 +48,65 @@ class CruiseControlVisualizer(QWidget):
     # Create the window and all elements within it
     def init_ui(self):
         # Set the window's size and title
-        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.setFixedSize(WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH)
         self.setWindowTitle('Cruise Control Visualizer')
         # Display the window on screen
         self.show()
+        self.update_ui((None, None))
 
     # Update the user interface using data retrieved from the data thread
     def update_ui(self, data):
         # Unwrap the data tuple
         speed, samples = data
-        print(speed)
+        # Temporarily create a random speed and sample
+        speed = random.random() / 2
+        samples = []
+        for _ in range(25):
+            samples.append(Sample(
+                angle=random.randint(0, 360000),
+                distance=random.randint(2000, 4000),
+                signal_strength=random.randint(0, 255)
+            ))
+        samples.sort(key=lambda x: x.angle)
+
+        # Clear the global list of sample points
+        self.sample_points = []
+
+        # Iterate over the samples
+        for sample in samples:
+            # On the circle centered at the origin with length of the distance multiplied by the scaling factor,
+            # find the point corresponding to the angle
+            # Do this with sine and cosine operations to calculate the corresponding point on the unit circle
+            # and multiply that by the distance and the scaling factor
+            # Finally, add half of the window's side length to center the points in the window
+            sample_point = [
+                (trig_function(math.radians(sample.angle))
+                 * sample.distance
+                 * DISTANCE_SCALING_FACTOR)
+                + (WINDOW_SIDE_LENGTH / 2)
+                for trig_function in (math.cos, math.sin)
+            ]
+            # Add the point to the global list
+            self.sample_points.append(sample_point)
+
+        # Redraw the window
+        self.repaint()
+
+    # Called when the window is redrawn and used to display all of the sample points on the window
+    def paintEvent(self, _):
+        # Create a painter and begin painting
+        painter = QPainter()
+        painter.begin(self)
+        # Paint the points in light gray with a light gray outline
+        painter.setBrush(Qt.lightGray)
+        painter.setPen(Qt.lightGray)
+
+        # Iterate over the sample points
+        for sample_point in self.sample_points:
+            # Convert the tuple to a QPoint
+            sample_qpoint = QPoint(*sample_point)
+            # Draw the point on screen
+            painter.drawEllipse(sample_qpoint, SAMPLE_POINT_DIAMETER, SAMPLE_POINT_DIAMETER)
 
 
 # The data thread that runs forever, accepting values from the LIDAR and displaying them on screen
