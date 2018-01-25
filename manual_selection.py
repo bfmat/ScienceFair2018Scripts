cda3e881cfcc39df411802728dfa2f31f7d5ef54
brendon-ai <brendon-m@outlook.com>
2018-01-25 17:53:23 -0500

    Simplified, generalized to stop sign task

--- manual_selection.py
+++ manual_selection.py
@@ -10,15 +10,15 @@ from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtWidgets import QLabel, QWidget, QApplication
 from skimage.io import imread, imsave
 
-# Program which allows a user to select the position of a road line in a wide image
+# Program which allows a user to select the position of an object in a wide image
 # in order to create training data for a convolutional neural network
 # Created by brendon-ai, September 2017
 
 
 # Width of the window
-WINDOW_WIDTH = 1200
+WINDOW_WIDTH = 960
 
-# Height of output training images (width is assumed to be the same as that of the input images)
+# Height of output training images (width is the same as that of the input images)
 OUTPUT_HEIGHT = 16
 
 
@@ -42,9 +42,9 @@ class ManualSelection(QWidget):
     # Factor by which images are scaled up to be displayed (computed at runtime)
     image_scaling_factor = None
 
-    # A list of two-dimensional points that the user places on the image to mark the road line
-    # Used as a path for positive training examples along the line
-    road_line_path = []
+    # A list of two-dimensional points that the user places on the image
+    # Used as positive training examples
+    selected_points = []
 
     # Call various initialization functions
     def __init__(self):
@@ -65,10 +65,11 @@ class ManualSelection(QWidget):
         image_height, image_width = imread(self.image_paths[0]).shape[:2]
 
         # Calculate the scale of the window compared to the image
-        window_scale = WINDOW_WIDTH / image_width
+        # This must be an integer
+        self.image_scaling_factor = WINDOW_WIDTH // image_width
 
         # Multiply the image height by the scale to get the window height
-        self.window_height = image_height * window_scale
+        self.window_height = image_height * self.image_scaling_factor
 
         # Set up the UI using the predefined width and computed height of the window
         self.init_ui()
@@ -101,7 +102,7 @@ class ManualSelection(QWidget):
             mouse_event.x() // self.image_scaling_factor,
             mouse_event.y() // self.image_scaling_factor
         )
-        self.road_line_path.append(mouse_position)
+        self.selected_points.append(mouse_position)
 
     # Listen for key presses and update the image when the space bar is pressed
     def keyPressEvent(self, key_event):
@@ -111,8 +112,8 @@ class ManualSelection(QWidget):
 
             # Save the current image data and clear the line path
             output_folder = os.path.expanduser(sys.argv[2])
-            save_training_data(self.current_image, self.road_line_path, output_folder)
-            self.road_line_path = []
+            save_training_data(self.current_image, self.selected_points, output_folder)
+            self.selected_points = []
 
             # If there are no images left that we haven't read, exit the application
             if self.current_image_index == len(self.image_paths):
@@ -133,11 +134,6 @@ class ManualSelection(QWidget):
         bytes_per_line = channel * width
         current_image_qimage = QImage(self.current_image.data, width, height, bytes_per_line, QImage.Format_RGB888)
         current_image_qpixmap = QPixmap(current_image_qimage).scaled(WINDOW_WIDTH, self.window_height)
-
-        # Set the image scaling factor to the window width divided by the image width
-        # This must be an integer for everything to work properly
-        # That is, the screen dimensions must be an even multiple of the input image dimensions
-        self.image_scaling_factor = WINDOW_WIDTH // width
 
         # Fill the image box with the picture
         self.image_box.setPixmap(current_image_qpixmap)
@@ -163,12 +159,12 @@ def get_image_paths(folder):
 
 
 # Slice and save data derived from a single image
-def save_training_data(image, road_line_path, output_folder):
-    # For every point defined on the road line, we take a slice of the image centered on the point's vertical value
-    for road_line_point in road_line_path:
+def save_training_data(image, selected_points, output_folder):
+    # For every defined point, we take a slice of the image centered on the point's vertical value
+    for selected_point in selected_points:
 
         # Calculate the top and bottom limits of the slice
-        vertical_slice_start = road_line_point[1] - (OUTPUT_HEIGHT // 2)
+        vertical_slice_start = selected_point[1] - (OUTPUT_HEIGHT // 2)
         vertical_slice_end = vertical_slice_start + OUTPUT_HEIGHT
 
         # Slice the image's NumPy array representation
@@ -177,7 +173,7 @@ def save_training_data(image, road_line_path, output_folder):
         # Only save the image if the height of the slice is as expected and it is not clipped by the edge of the image
         if image_slice.shape[0] == OUTPUT_HEIGHT:
             # Use the Unix time and the position of the current road line point in the file name
-            file_name = '{}/x{}_y{}_{}.png'.format(output_folder, road_line_point[0], road_line_point[1], uuid.uuid4())
+            file_name = '{}/x{}_y{}_{}.png'.format(output_folder, selected_point[0], selected_point[1], uuid.uuid4())
 
             # Save the image
             imsave(file_name, image_slice)
