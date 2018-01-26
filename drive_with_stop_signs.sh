43070f57e93445a13275cfd50bb7f6939540e8db
brendon-ai <brendon-m@outlook.com>
2018-01-26 10:48:51 -0500

    Updated network path

--- drive_with_stop_signs.sh
+++ drive_with_stop_signs.sh
@@ -11,7 +11,7 @@ python2 -m LaneDetection.apply.simulation_stream \
 ~/Developer/Models/TrainedModels/batch=1513889469-epoch=10-val_loss=0.086984.h5 &
 
 # Run the stop sign finding script in the foreground
-python3 -m StopSignFinder.infer ~/Developer/Models/StopSignModels/Initial.h5
+python3 -m StopSignFinder.infer ~/Developer/Models/StopSignModels/TrainedWithNegative.h5
 
 # When the foreground process is interrupted, kill the background process
 pkill Python
