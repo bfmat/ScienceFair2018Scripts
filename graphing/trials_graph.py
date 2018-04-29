fc9d7edddb6f997521318e2446891c96543af622
brendon-ai <brendon-m@outlook.com>
2018-04-29 16:57:39 -0400

    Removed arbitrary scale for deep Q-network

--- graphing/trials_graph.py
+++ graphing/trials_graph.py
@@ -15,7 +15,8 @@ COLORS = ['red', 'blue']
 x_label = input('Enter the X axis label: ')
 # Get the number of trials, and the number of data series per trial, from the user
 num_trials = int(input('Enter the number of trials: '))
-num_series = 2 if input('Enter "y" if two data series are being used: ') == 'y' else 1
+num_series = 2 if input(
+    'Enter "y" if two data series are being used: ') == 'y' else 1
 # Get the numbers of the trials
 print('Enter the numbers of the {} trials, each on a separate line:'.format(num_trials))
 trial_numbers = [int(input().strip()) for _ in range(num_trials)]
@@ -25,7 +26,8 @@ values_by_trial = {}
 # Iterate over the subplots and corresponding trial names
 for series_index in range(num_series):
     # Ask the user for the name of this data series
-    series_name = input('Enter the name of data series {}: '.format(series_index))
+    series_name = input(
+        'Enter the name of data series {}: '.format(series_index))
     # Collect every data point in this series, each on a separate line
     print('Enter the {} data points, each on a separate line:'.format(num_trials))
     series_values = [float(input().strip()) for _ in range(num_trials)]
@@ -50,17 +52,15 @@ for subplot, series_name, color in zip(subplots, values_by_trial.keys(), COLORS)
         label=series_name
     )
     # Set the Y axis label with the series name
-    subplot.set_ylabel(series_name, fontsize=AXIS_LABEL_SIZE, labelpad=AXIS_LABEL_PADDING)
+    subplot.set_ylabel(series_name, fontsize=AXIS_LABEL_SIZE,
+                       labelpad=AXIS_LABEL_PADDING)
     # Set the X axis label with the specified X axis label
-    subplot.set_xlabel(x_label, fontsize=AXIS_LABEL_SIZE, labelpad=AXIS_LABEL_PADDING)
+    subplot.set_xlabel(x_label, fontsize=AXIS_LABEL_SIZE,
+                       labelpad=AXIS_LABEL_PADDING)
     # Set the tick font size
     subplot.tick_params(labelsize=TICK_LABEL_SIZE, pad=TICK_LABEL_PADDING)
     # Create a color handle for the legend
     color_handles.append(mpatches.Patch(color=color, label=series_name))
-
-# Put axis limits for the deep Q-network graph into place
-plt.xlim(xmin=0)
-plt.ylim(ymin=0, ymax=10000)
 
 # Automatically generate a legend for the graph if there is more than one data series
 if num_series > 1:
